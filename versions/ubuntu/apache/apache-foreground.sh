27f9e150d980c551258e4f220ec0dfe6bc0f09d2
Jan Thoennessen <jan.thoennessen@cbc.de>
2019-03-13 17:49:52 +0100

    restructured

---
+++ versions/ubuntu/apache/apache-foreground.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# Apache gets grumpy about PID files pre-existing
+: "${APACHE_RUN_DIR:=/var/run/apache2}"
+: "${APACHE_PID_FILE:=$APACHE_RUN_DIR/apache2.pid}"
+rm -f "$APACHE_PID_FILE"
+
+exec apache2 -DFOREGROUND "$@"
